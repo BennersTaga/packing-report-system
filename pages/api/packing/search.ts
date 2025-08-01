565a56f305714135828b79e2180ce03c31eb47ce
BennersTaga <s-taga@benners.co.jp>
2025-08-01 13:43:44 +0900

    Update search.ts

--- pages/api/packing/search.ts
+++ pages/api/packing/search.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { searchPackingData } from '../../../lib/google-sheets';
+import { searchPackingData } from '../../../lib/gas-sheets';
 
 export default async function handler(
   req: NextApiRequest,
